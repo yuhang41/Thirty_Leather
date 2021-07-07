3ab195d324b12dc2204f0ad359e8e1cbfb590548
Yuhang <etc70129@gmail.com>
2021-07-07 15:56:39 +0800

    7/7 1556

--- js/shopping-step3.js
+++ js/shopping-step3.js
@@ -1,10 +1,8 @@
 let order_total = document.querySelector('.order-total');
-let order_commodity = document.querySelector('.order-commodity');
-let commodity_content = document.querySelector('.commodity-content');
+// let order_commodity = document.querySelector('.order-commodity');
+// let commodity_content = document.querySelector('.commodity-content');
 
 function order_totalHandler(){
-    order_total.classList.toggle('active');
-    order_commodity.classList.toggle('active');
-    commodity_content.classList.toggle('active');
+    order_total.classList.toggle('active');    
 }
 order_total.addEventListener('click',order_totalHandler);
