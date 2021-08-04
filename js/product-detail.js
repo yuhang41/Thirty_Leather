f2b3a2c5f9e11ed8df49dc29c871f2f8ee6d213b
seesawjack <82428741+seesawjack@users.noreply.github.com>
2021-08-04 21:05:31 +0800

    08/04 21:05
    修正:
    商品頁&單品頁修正

--- js/product-detail.js
+++ js/product-detail.js
@@ -199,11 +199,18 @@ function everyStyle(data,display,background,className){
 //navbar
 //When the user scrolls the page, execute myFunction
 window.onscroll = function(){myFunction()};
-//取得banner的高度
-var bannerHeight = document.querySelector('.swiper-container').offsetHeight;
+//取得螢幕的高度
+var  screenHeight = screen.height;
+
 let backTopBtn = document.querySelector('.back-top');
+let commodityOpt = document.querySelector('.all-commodity');
 function myFunction() {
-  if (window.pageYOffset >= bannerHeight) {
+  if(window.pageYOffset >= screenHeight/2){
+    commodityOpt.classList.add("sticky");
+  }else{
+    commodityOpt.classList.remove("sticky");
+  }
+  if (window.pageYOffset >= screenHeight) {
     backTopBtn.classList.add("sticky");
   } else {
     backTopBtn.classList.remove("sticky");
